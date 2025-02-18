ec2a5da2d75a1f96397f299bb9c4398d34a4341d
MarJuice <mariushanss1@gmail.com>
2025-02-18 11:09:21 +0100

    Create function outlines

--- task1.mjs
+++ task1.mjs
@@ -1,10 +1,33 @@
 //#region TASK: Functions are a popping
 
 // A function that returns the square of a number
+function square(num) {
+
+}
+
 // A function that returns a length in mm assuming it has been given a length in inches.
+function inchToMilimeter(num) {
+
+}
+
 // A function that returns the root of a number
+function root(num) {
+
+}
+
 // A function that returns the cube of a number
+function cube(num) {
+
+}
+
 // A function that returns the area of a circle given the radius.
+function area(rad) {
+
+}
+
 // A function that returns a greeting, given a name.
+function greet(name) {
+    
+}
 
 //#endregion

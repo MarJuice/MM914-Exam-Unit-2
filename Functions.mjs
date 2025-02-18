2267632adcc488d5036daf4337fec757e6ee55f5
MarJuice <mariushanss1@gmail.com>
2025-02-18 11:02:46 +0100

    Create functions file

---
+++ Functions.mjs
@@ -0,0 +1,41 @@
+//#region TASK: Functions are a popping
+
+// A function that returns the square of a number
+// A function that returns a length in mm assuming it has been given a length in inches.
+// A function that returns the root of a number
+// A function that returns the cube of a number
+// A function that returns the area of a circle given the radius.
+// A function that returns a greeting, given a name.
+
+//#endregion
+
+
+//#region TASK: Flatten those numbers
+
+// A function that returns a flattened version of any array structure
+
+//#endregion
+
+
+//#region TASK: Left and right up and down, away we go
+
+// Calculate the sum of the full structure.
+// Report the deepest level of the structure.
+// Report the number of nodes.
+
+//#endregion
+
+
+//#region TASK: My books they are a mess
+
+// Return only books starting with The
+// Return only books written by authors with a t in their name
+// The number of books written after 1992
+// The number of books written before 2004
+// Return the isbn number of all the books for a given author.
+// List books alphabetically assending or decendig
+// List books chronologically assending or decendig
+// List books grouped by author last name
+// Lits books grouped by author first name
+
+//#endregion 

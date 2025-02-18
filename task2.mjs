c7284ab82da651236bb5609523f7ff2a66f950c2
MarJuice <mariushanss1@gmail.com>
2025-02-18 11:04:37 +0100

    Create task files

---
+++ task2.mjs
@@ -0,0 +1,5 @@
+//#region TASK: Flatten those numbers
+
+// A function that returns a flattened version of any array structure
+
+//#endregion
---
+++ task3.mjs
@@ -0,0 +1,7 @@
+//#region TASK: Left and right up and down, away we go
+
+// Calculate the sum of the full structure.
+// Report the deepest level of the structure.
+// Report the number of nodes.
+
+//#endregion

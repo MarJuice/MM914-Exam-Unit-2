59d8747221319d06fd892e553fb8ea215123d023
MarJuice <mariushanss1@gmail.com>
2025-02-18 18:59:43 +0100

    Create flatten function with flat method

--- task2.mjs
+++ task2.mjs
@@ -1,5 +1,14 @@
+// Function test:
+
+import array from "./arrays.json" with { type: "json" }
+
+console.log(flatten(array));
+
 //#region TASK: Flatten those numbers
 
 // A function that returns a flattened version of any array structure
+function flatten(array) {
+    return array.flat(Infinity);
+}
 
 //#endregion

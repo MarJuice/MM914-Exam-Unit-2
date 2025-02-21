e5790065fd3703b9cc91df8f08dd346a45b1af32
Marius <79099572+MarJuice@users.noreply.github.com>
2025-02-21 15:46:01 +0100

    Create sum and check functions

--- task3.mjs
+++ task3.mjs
@@ -1,7 +1,39 @@
+// Function tests:
+
+import node from "./nodes.json" with { type: "json" };
+
+console.log(sum(node));
+
 //#region TASK: Left and right up and down, away we go
 
-// Calculate the sum of the full structure.
-// Report the deepest level of the structure.
-// Report the number of nodes.
+function sum(node) {
+    let sum = 0;
+    let deepest = 0;
+    let nodes = 0;
+
+    // Report the deepest level
+    function check(node, level) {
+        if (level > deepest) {
+            deepest = level;
+        }
+
+        // Calculate the sum
+        sum += node.value;
+        nodes++;
+
+        // Count the number of nodes
+        if (node.left) {
+            check(node.left, level + 1);
+        }
+
+        if (node.right) {
+            check(node.right, level + 1);
+        }
+    }
+
+    check(node, 0);
+
+    return { sum, deepest, nodes };
+}
 
 //#endregion
